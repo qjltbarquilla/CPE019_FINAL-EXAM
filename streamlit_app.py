650e6228693757eef82fe041afaca284839329fd
qjltbarquilla <qjltbarquilla@tip.edu.ph>
2025-05-18 19:18:57 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ uploaded_file = st.file_uploader("Upload a weather image", type=["jpg", "png", "
 
 if uploaded_file is not None:
     img = Image.open(uploaded_file).resize((150, 150))
-    st.image(img, caption='Uploaded Image', use_column_width=True)
+    st.image(img, caption='Uploaded Image', use_container_width=True)
 
     img_array = image.img_to_array(img)
     img_array = np.expand_dims(img_array, axis=0) / 255.0
